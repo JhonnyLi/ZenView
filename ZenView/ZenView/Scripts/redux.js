2938b5039f9260d2bb1883c94385c7bb645ce22c
Jhonny Li <jhonny@camelonta.se>
2019-03-25 12:57:17 +0100

    Redux state test

--- ZenView/ZenView/Scripts/redux.js
+++ ZenView/ZenView/Scripts/redux.js
@@ -2,9 +2,23 @@
 
 Main.Redux = (function () {
     var reducer = function reducer(state, action) {
-        if (state === undefined) state = 0;
-
-        if (action === undefined) return state;
+        debugger;
+        if (state === undefined) {
+            $.getJSON('https://zenview.zendesk.com/api/v2/search.json?query=type:ticket status:open').done(function (data) {
+                console.log(data);
+            }).fail(function (err) {
+                console.log(err);
+            });
+            //$.ajax({
+            //    url: 'https://zenview.zendesk.com/api/v2/search.json?query=type:ticket status:open',
+            //    contentType: 'application/json',
+            //    type: 'GET'
+            //}).done(function (data) {
+            //    console.log(data.results);
+            //    state = data.results;
+            //});
+            return state;
+        }
         console.log("Reducers reduce");
         switch (action.type) {
             case 'INCREMENT':
