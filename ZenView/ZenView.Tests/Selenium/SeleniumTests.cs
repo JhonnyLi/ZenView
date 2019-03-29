30227aeae36c24d8060cff6f4a993d6e10c30761
Jhonny Li <jhonny@jhonny.se>
2019-03-29 02:37:43 +0100

    Added more Selenium tests

--- ZenView/ZenView.Tests/Selenium/SeleniumTests.cs
+++ ZenView/ZenView.Tests/Selenium/SeleniumTests.cs
@@ -1,24 +1,84 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ZenView;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace ZenView.Tests.Selenium
 {
     [TestClass]
     public class SeleniumTests
     {
+        private IWebDriver driver;
+        private string appURL;
+
+
         [TestMethod]
-        public void SeleniumTrial()
+        public void SeleniumNavigateAbout()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://zenview.azurewebsites.net/");
-            driver.FindElement(By.Id("signalRbtn")).Click();
+            driver.Navigate().GoToUrl(appURL);
+            driver.FindElement(By.LinkText("About")).Click();
+            Assert.AreEqual(driver.Title, "About - ZenView");
+        }
+        
+        [TestMethod]
+        public void SeleniumNavigateContact()
+        {
+            driver.Navigate().GoToUrl(appURL);
+            driver.FindElement(By.LinkText("Contact")).Click();
+            Assert.AreEqual(driver.Title, "Contact - ZenView");
+        }
+
+        [TestMethod]
+        public void SeleniumNavigateRegister()
+        {
+            driver.Navigate().GoToUrl(appURL);
+            driver.FindElement(By.LinkText("Register")).Click();
+            Assert.AreEqual(driver.Title, "Register - ZenView");
+        }
+
+        [TestMethod]
+        public void SeleniumNavigateLogin()
+        {
+            driver.Navigate().GoToUrl(appURL);
+            driver.FindElement(By.LinkText("Log in")).Click();
+            Assert.AreEqual(driver.Title, "Log in - ZenView");
+        }
+
+        [TestMethod]
+        public void SeleniumExecuteMainInit()
+        {
+            driver.Navigate().GoToUrl(appURL);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("return console.log(Main.Init())");
+        }
+        
+        [TestInitialize()]
+        public void Selenium()
+        {
+            appURL = "https://zenview.azurewebsites.net/";
+
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "Chrome":
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
+
+
+        [TestCleanup()]
+        public void SeleniumCleanUp()
+        {
             driver.Quit();
         }
 
